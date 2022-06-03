c909b582e68b3d72b47f8f54cef071979e73453b
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-06-03 12:50:48 -0500

    Create deep_neural_network_testing.py

---
+++ deep_neural_network_testing.py
@@ -0,0 +1,105 @@
+#@title Import relevant modules
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+
+# The following lines adjust the granularity of reporting. 
+pd.options.display.max_rows = 10
+pd.options.display.float_format = "{:.1f}".format
+
+# The following line improves formatting when ouputting NumPy arrays.
+np.set_printoptions(linewidth = 200)
+
+def plot_curve(epochs, hist, list_of_metrics):
+  """Plot a curve of one or more classification metrics vs. epoch."""  
+  # list_of_metrics should be one of the names shown in:
+  # https://www.tensorflow.org/tutorials/structured_data/imbalanced_data#define_the_model_and_metrics  
+
+  plt.figure()
+  plt.xlabel("Epoch")
+  plt.ylabel("Value")
+
+  for m in list_of_metrics:
+    x = hist[m]
+    plt.plot(epochs[1:], x[1:], label=m)
+
+  plt.legend()
+  plt.show()
+
+print("Loaded the plot_curve function.")
+
+def create_model(my_learning_rate):
+  """Create and compile a deep neural net."""
+  
+  # All models in this course are sequential.
+  model = tf.keras.models.Sequential()
+
+  # The features are stored in a two-dimensional 28X28 array. 
+  # Flatten that two-dimensional array into a a one-dimensional 
+  # 784-element array.
+  model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+
+  # Define the first hidden layer.   
+  model.add(tf.keras.layers.Dense(units=32, activation='relu'))
+  
+  # Define a dropout regularization layer. 
+  model.add(tf.keras.layers.Dropout(rate=0.2))
+
+  # Define the output layer. The units parameter is set to 10 because
+  # the model must choose among 10 possible output values (representing
+  # the digits from 0 to 9, inclusive).
+  #
+  # Don't change this layer.
+  model.add(tf.keras.layers.Dense(units=10, activation='softmax'))     
+                           
+  # Construct the layers into a model that TensorFlow can execute.  
+  # Notice that the loss function for multi-class classification
+  # is different than the loss function for binary classification.  
+  model.compile(optimizer=tf.keras.optimizers.Adam(lr=my_learning_rate),
+                loss="sparse_categorical_crossentropy",
+                metrics=['accuracy'])
+  
+  return model    
+
+def train_model(model, train_features, train_label, epochs,
+                batch_size=None, validation_split=0.1):
+  """Train the model by feeding it data."""
+
+  history = model.fit(x=train_features, y=train_label, batch_size=batch_size,
+                      epochs=epochs, shuffle=True, 
+                      validation_split=validation_split)
+ 
+  # To track the progression of training, gather a snapshot
+  # of the model's metrics at each epoch. 
+  epochs = history.epoch
+  hist = pd.DataFrame(history.history)
+
+  return epochs, hist    
+
+df = pd.read_csv(r"C:\Users\KSpicer\Desktop\victorian_era_authorship_attribution_project\dataset\Gungor_2018_VictorianAuthorAttribution_data-train.csv", encoding='latin-1')
+
+X = df.text
+y = df.author
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, random_state=1)
+
+vectorizer = CountVectorizer()
+vector.fit(x_train)
+X_train_dtm = vector.transform(x_test)
+
+learning_rate = 0.003
+epochs = 50
+batch_size = 4000
+validation_split = 0.2
+
+my_model = create_model(learning_rate)
+epochs, hist = train_model(my_model, X_train_dtm, y_train, epochs, batch_size, validation_split)
+
+list_of_metrics_to_plot = ['accuracy']
+plot_curve(epochs, hist, list_of_metrics_to_plot)
+print("\n Evaluate the new model against the test set: ")
+my_model.evaluate(x = x_test_normalized, y = y_test, batch_size = batch_size)
