de3d5537be4dd2ffac53b5ef43c39731c5af4c15
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-06-09 15:00:17 -0500

    updates

---
+++ build_model.py
@@ -0,0 +1,95 @@
+from tensorflow.python.keras import models
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Dropout
+from tensorflow.python.keras import models
+from tensorflow.python.keras import initializers
+from tensorflow.python.keras import regularizers
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Dropout
+from tensorflow.python.keras.layers import Embedding
+from tensorflow.python.keras.layers import SeparableConv1D
+from tensorflow.python.keras.layers import MaxPooling1D
+from tensorflow.python.keras.layers import GlobalAveragePooling1D
+import vectorize_data
+
+def get_last_layer_units_and_activation(num_classes):
+    if num_classes == 2:
+        activation = 'sigmoid'
+        units = 1
+    else:
+        activation = 'softmax'
+        units = num_classes
+    return units, activation
+
+def mlp_model(layers, units, dropout_rate, input_shape, num_classes):
+    op_units, op_activation = get_last_layer_units_and_activation(num_classes)
+    model = models.Sequential
+    model.add(Dropout(rate=dropout_rate, input_shape=input_shape))
+
+    for _ in range(layers-1):
+        model.add(Dense(units=units, activation='relu'))
+        model.add(Dropout(rate=dropout_rate))
+    
+    model.add(Dense(units=op_units, activation=op_activation))
+    return model
+
+def sepcnn_model(blocks,
+                 filters,
+                 kernel_size,
+                 embedding_dim,
+                 dropout_rate,
+                 pool_size,
+                 input_shape,
+                 num_classes,
+                 num_features,
+                 use_pretrained_embedding=False,
+                 is_embedding_trainable=False,
+                 embedding_matrix=None):
+    op_units, op_activation = get_last_layer_units_and_activation(num_classes)
+    model = models.Sequential()
+
+    # Add embedding layer. If pre-trained embedding is used add weights to the
+    # embeddings layer and set trainable to input is_embedding_trainable flag.
+    if use_pretrained_embedding:
+        model.add(Embedding(input_dim=num_features,
+                            output_dim=embedding_dim,
+                            input_length=input_shape[0],
+                            weights=[embedding_matrix],
+                            trainable=is_embedding_trainable))
+    else:
+        model.add(Embedding(input_dim=num_features,
+                            output_dim=embedding_dim,
+                            input_length=input_shape[0]))
+
+    for _ in range(blocks-1):
+        model.add(Dropout(rate=dropout_rate))
+        model.add(SeparableConv1D(filters=filters,
+                                  kernel_size=kernel_size,
+                                  activation='relu',
+                                  bias_initializer='random_uniform',
+                                  depthwise_initializer='random_uniform',
+                                  padding='same'))
+        model.add(SeparableConv1D(filters=filters,
+                                  kernel_size=kernel_size,
+                                  activation='relu',
+                                  bias_initializer='random_uniform',
+                                  depthwise_initializer='random_uniform',
+                                  padding='same'))
+        model.add(MaxPooling1D(pool_size=pool_size))
+
+    model.add(SeparableConv1D(filters=filters * 2,
+                              kernel_size=kernel_size,
+                              activation='relu',
+                              bias_initializer='random_uniform',
+                              depthwise_initializer='random_uniform',
+                              padding='same'))
+    model.add(SeparableConv1D(filters=filters * 2,
+                              kernel_size=kernel_size,
+                              activation='relu',
+                              bias_initializer='random_uniform',
+                              depthwise_initializer='random_uniform',
+                              padding='same'))
+    model.add(GlobalAveragePooling1D())
+    model.add(Dropout(rate=dropout_rate))
+    model.add(Dense(op_units, activation=op_activation))
+    return model
