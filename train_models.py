0e50e704bb2db7bcf7600047efffdc850f054065
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-06-10 14:57:00 -0500

    deletoin and more updates

--- train_models.py
+++ train_models.py
@@ -6,6 +6,24 @@ import build_model
 import load_and_shuffle_data
 import explore_data
 
+def plot_model_loss(model_name, string_1='loss', string_2='val_loss'):
+    plt.plot(model_name.history[string_1])
+    plt.plot(model_name.history[string_2])
+    plt.title('model loss')
+    plt.ylabel(string_1)
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+def plot_model_accuracy(model_name, string_1='accuracy', string_2='val_accuracy'):
+    plt.plot(model_name.history[string_1])
+    plt.plot(model_name.history[string_2])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='lower right')
+    plt.show()
+
 def train_ngram_model(data,
                       learning_rate=1e-3,
                       epochs=50,
