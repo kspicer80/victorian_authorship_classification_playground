b2b64d347e2be5139dbfa6f8c97222f174c8e02a
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-06-14 12:58:03 -0500

    updated metrics

--- multiclass.py
+++ multiclass.py
@@ -13,8 +13,20 @@ from sklearn.model_selection import cross_val_score
 from sklearn.metrics import confusion_matrix
 from sklearn import metrics
 import seaborn as sns
+import urllib.request
+from io import StringIO, BytesIO, TextIOWrapper
+from zipfile import ZipFile
 
-df = pd.read_csv("/Users/spicy.kev/Desktop/victorian_authorship/victorian_training_data.csv", encoding='latin-1', low_memory=False)
+uci_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00454/"
+victorian_authorship_file = "dataset.zip"
+
+response = urllib.request.urlopen(uci_url + urllib.request.quote(victorian_authorship_file))
+
+zipfile = ZipFile(BytesIO(response.read()))
+
+data = TextIOWrapper(zipfile.open('dataset/Gungor_2018_VictorianAuthorAttribution_data-train.csv'))
+
+df = pd.read_csv(data, encoding='latin-1', low_memory=False)
 #X_sample = df.sample(frac=.25, replace=False, random_state=1)
 X = df.text
 y = df.author
@@ -24,12 +36,11 @@ tfidf = TfidfVectorizer(sublinear_tf=True, min_df=20, ngram_range=(1,2), stop_wo
 features = tfidf.fit_transform(X)
 
 labels = df.author
-'''
+
 models = [
     RandomForestClassifier(n_estimators=100, max_depth=5, random_state=0),
     LinearSVC(),
     MultinomialNB(),
-    LogisticRegression(random_state=0),
 ]
 
 CV = 5
@@ -55,30 +66,34 @@ for model in models:
 '''
 X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.20, random_state=1)
 
-#svc_model = LinearSVC()
-#svc_model.fit(X_train, y_train)
-#y_pred = svc_model.predict(X_test)
-#print("Classification Report Metrics:\n")
-#print(metrics.classification_report(y_test, y_pred))
+svc_model = LinearSVC()
+svc_model.fit(X_train, y_train)
 
-#confusion_matrix = confusion_matrix(y_test, y_pred)
-#fig, ax = plt.subplots(figsize=(20, 10))
-#sns.heatmap(confusion_matrix, annot=True, cmap='Blues', fmt='d')
-#plt.ylabel('Actual')
-#plt.xlabel('Predicted')
-#plt.title("Confusion Matrix for Linear SCV")
-#plt.show()
+y_pred = svc_model.predict(X_test)
 
-rf = RandomForestClassifier()
+print("Classification Report Metrics:\n")
+print(metrics.classification_report(y_test, y_pred))
+
+confusion_matrix = confusion_matrix(y_test, y_pred)
+fig, ax = plt.subplots(figsize=(20, 10))
+sns.heatmap(confusion_matrix, annot=True, cmap='Blues', fmt='d')
+plt.ylabel('Actual')
+plt.xlabel('Predicted')
+plt.title("Confusion Matrix for Linear SCV")
+plt.show()
+
+rf = RandomForestClassifier(n_estimators = 150, max_depth=10, random_state=1)
 print(rf.get_params())
-
 rf.fit(X_train, y_train)
 y_pred = rf.predict(X_test)
-
-# 3. Calculate Precision and Recall scores and the Confusion Matrix
 print(rf.score(X_test, y_test).round(4))
-
 conf_mat = confusion_matrix(y_test, y_pred)
 print(confusion_matrix(y_test, y_pred))
-
+fig, ax = plt.subplots(figsize=(20, 10))
+sns.heatmap(conf_mat, annot=True, cmap='Blues', fmt='d')
+plt.ylabel('Actual')
+plt.xlabel('Predicted')
+plt.title("Confusion Matrix for Linear SCV")
+plt.show()
+'''
 
